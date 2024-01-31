a1ac298f2ad836c007d74c0b212f658387fa3110
Olival Paulino <olivalpaulinocc@gmail.com>
2024-01-31 12:17:49 -0300

    primeiro commit

---
+++ src/main/java/br/com/dobackaofront/sistemadevendas/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dobackaofront.sistemadevendas;
+
+/**
+ *
+ * @author Núcleo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
